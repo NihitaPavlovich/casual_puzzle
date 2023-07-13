8f83dd5813490f50b4a18898d9557f4e4f436573
Nikita Lozinsky <125041839+NihitaPavlovich@users.noreply.github.com>
2023-07-14 00:32:48 +0300

    13

---
+++ Assets/rBackToMenu.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class rBackToMenu : MonoBehaviour
+{
+    public void SwitchToScene0()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
